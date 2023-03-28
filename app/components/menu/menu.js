654e7bd91349d2063d8cc67cd983f094cfd53ded
Lucian_LV <lucian@bookster.ro>
2023-03-29 00:29:38 +0300

    try init

--- app/components/menu/menu.js
+++ app/components/menu/menu.js
@@ -1,4 +1,36 @@
 angular.module('app')
+    .controller("appController", function() {
+        $scope.init = function () {
+            debugger;
+            window.addEventListener('load', () => {
+                setTimeout(() => {
+                    let portfolioContainer = select('.portfolio-container');
+                    if (portfolioContainer) {
+                        let portfolioIsotope = new Isotope(portfolioContainer, {
+                            itemSelector: '.portfolio-item'
+                        });
+        
+                        let portfolioFilters = select('#portfolio-filter-category li', true);
+        
+                        on('click', '#portfolio-filter-category li', function (e) {
+                            e.preventDefault();
+                            portfolioFilters.forEach(function (el) {
+                                el.classList.remove('filter-active');
+                            });
+                            this.classList.add('filter-active');
+        
+                            portfolioIsotope.arrange({
+                                filter: this.getAttribute('data-filter')
+                            });
+                            portfolioIsotope.on('arrangeComplete', function () {
+                                AOS.refresh()
+                            });
+                        }, true);
+                    }
+                }, 1500);
+            });
+        }
+    })
     .component('menu', {
         templateUrl: "app/components/menu/menu.html",
         controller: ['$scope', '$translate', function ($scope, $translate) {
