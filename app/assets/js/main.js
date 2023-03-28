8aeec0900e01e277ca734d16585a2ff08d1925f1
Lucian_LV <lucian@bookster.ro>
2023-03-29 00:20:59 +0300

    try again

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -122,32 +122,33 @@
   // /**
   //  * Porfolio isotope and filter
   //  */
+    // window.addEventListener('load', () => {
+    //     setTimeout(() => {
+    //         let portfolioContainer = select('.portfolio-container');
+    //         if (portfolioContainer) {
+    //             let portfolioIsotope = new Isotope(portfolioContainer, {
+    //                 itemSelector: '.portfolio-item'
+    //             });
 
-  // on('click', '#portfolio-filter-category li', function (e) {
-  //   e.preventDefault();
-  //   debugger;
-  //   let portfolioContainer = select('.portfolio-container');
-  //   if (portfolioContainer) {
-  //       let portfolioIsotope = new Isotope(portfolioContainer, {
-  //           itemSelector: '.portfolio-item'
-  //       });
+    //             let portfolioFilters = select('#portfolio-filter-category li', true);
 
-  //       let portfolioFilters = select('#portfolio-filter-category li', true);
-        
-  //       portfolioFilters.forEach(function (el) {
-  //         el.classList.remove('filter-active');
-  //       });
+    //             on('click', '#portfolio-filter-category li', function (e) {
+    //                 e.preventDefault();
+    //                 portfolioFilters.forEach(function (el) {
+    //                     el.classList.remove('filter-active');
+    //                 });
+    //                 this.classList.add('filter-active');
 
-  //       this.classList.add('filter-active');
-    
-  //       portfolioIsotope.arrange({
-  //           filter: this.getAttribute('data-filter')
-  //       });
-  //       portfolioIsotope.on('arrangeComplete', function () {
-  //           AOS.refresh()
-  //       });
-  //   }
-  // }, true);
+    //                 portfolioIsotope.arrange({
+    //                     filter: this.getAttribute('data-filter')
+    //                 });
+    //                 portfolioIsotope.on('arrangeComplete', function () {
+    //                     AOS.refresh()
+    //                 });
+    //             }, true);
+    //         }
+    //     }, 1500);
+    // });
 
   /**
    * Animation on scroll
