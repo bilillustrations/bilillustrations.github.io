d094f20f157df70a03f70256031220126584b9c8
Lucian_LV <lucian@bookster.ro>
2023-04-01 23:45:35 +0300

    unfix timm

--- app/components/portfolio/portfolio.js
+++ app/components/portfolio/portfolio.js
@@ -23,7 +23,7 @@ angular.module('app')
             { id: 6, categoryId: 2, slug: 'improvesti', order: 6 },
             { id: 7, categoryId: 2, slug: 'rebel-girls', order: 7, images: [12, 12, 12, 6, 6, 6, 6]  },
             { id: 8, categoryId: 3, slug: 'teds-coffee', order: 1, images: [12, 8, 4, 4, 8, 12, 12, 4, 8, 6, 6, 8, 4] },
-            { id: 9, categoryId: 2, slug: 'timm-thaler', order: 9, images: [6, 6, 5, 7, 7, 5, 5, 7, 7, 5, 4, 4, 4, 6, 6, 7, 5, 6, 6] },
+            { id: 9, categoryId: 2, slug: 'timm-thaler', order: 9, images: [6, 6, 4, 8, 8, 4, 4, 8, 8, 4, 4, 4, 4, 6, 6, 8, 4, 6, 6] },
             { id: 10, categoryId: 1, slug: 'inktober', order: 2, images: [6,6,4,4,4,12,6,6,6,6,6,6,4,4,4,4,4,4,12,6,6,6,6,6,6,6,6,6,6,6,6] },
             { id: 10, categoryId: 1, slug: '5-poems', order: 2, images: [6, 6, 12] }
         ]
