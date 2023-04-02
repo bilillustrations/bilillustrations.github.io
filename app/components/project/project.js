7cd74cad2cffba42e97f1b1dca9b2922830c48e1
Lucian_LV <lucian@bookster.ro>
2023-04-02 16:11:05 +0300

    limit

--- app/components/project/project.js
+++ app/components/project/project.js
@@ -32,32 +32,27 @@ angular.module('app')
         var link = window.location.href.toString();
         var initpath = "?";
 
-        $scope.images = [];
-        $scope.project = {
-            slug: "",
-            images: []
-        };
         $scope.take = 6;
-        $scope.skip = 0;
+        $scope.limit = 6;
        
         if(link.indexOf(initpath) > 0) {
             var path = link.substring(link.indexOf(initpath) + 1);
-debugger;
+
             for (var i = 0; i < projectsFactory.length; i++) {
                 if (projectsFactory[i].slug == path) {
                     $scope.hasError = false;
-
-                    $scope.project.slug = projectsFactory[i].slug;
-                    $scope.images = projectsFactory[i].images;
+                    $scope.project = projectsFactory[i];
+                    // $scope.project.slug = projectsFactory[i].slug;
+                    // $scope.images = projectsFactory[i].images;
                     
-                    var min = $scope.images.length;
-                    if(min > $scope.take) {
-                        min = $scope.take;
-                    }
+                    // var min = $scope.images.length;
+                    // if(min > $scope.take) {
+                    //     min = $scope.take;
+                    // }
 
-                    for(var j = 0; j < min; j++) {
-                        $scope.project.images.push($scope.images[j])
-                    }    
+                    // for(var j = 0; j < min; j++) {
+                    //     $scope.project.images.push($scope.images[j])
+                    // }    
 
                     break;
                 }
@@ -66,46 +61,14 @@ debugger;
 
         // this function fetches a random text and adds it to array
         $scope.more = function () {
-            var start = $scope.project.images.length;
-
-            if(start < $scope.images.length) {
-                var min = $scope.images.length - start;
-                if(min > $scope.take) {
-                    min = $scope.take;
-                }
-
-                for(var i = start; i < start + min; i++) {
-                    $scope.project.images.push($scope.images[i]);
-                }
+            var min = $scope.project.images.length - $scope.limit;
+            if(min > $scope.take) {
+                min = $scope.take;
             }
+            $scope.limit += min;
         };
-
-       // $scope.more();
-
-
 
         $scope.getClassName = function(p) {
             return "col-sm-"+p;
         }
-    }])
-    .directive("whenScrolled", function () {
-        return {
-
-            restrict: 'A',
-            link: function (scope, elem, attrs) {
-
-                // we get a list of elements of size 1 and need the first element
-                raw = document;//elem[0];
-
-                // we load more elements when scrolled past a limit
-                elem.bind("scroll", function () {
-                    scope.$apply(attrs.whenScrolled);
-                    if (raw.scrollTop + raw.offsetHeight + 5 >= raw.scrollHeight) {
-                        
-                        // we can give any function which loads more elements into the list
-                        //scope.$apply(attrs.whenScrolled);
-                    }
-                });
-            }
-        }
-    });
+    }]);
