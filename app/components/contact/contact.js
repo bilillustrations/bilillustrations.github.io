5b5c443f263c010a121a0e1c6b43d83616746074
Lucian Voica <lucianbookster@outlook.com>
2023-10-23 10:55:06 +0300

    contact sent

--- app/components/contact/contact.js
+++ app/components/contact/contact.js
@@ -2,10 +2,9 @@ angular.module('app')
     .component('contact', {
         templateUrl: "app/components/contact/contact.html",
         controller: ['$scope', function ($scope) {
+            $scope.messageSent = false;
             $scope.resetForm = function() {
-                document.getElementById("entry.993355435").value = "";
-                document.getElementById("entry.488430138").value = "";
-                document.getElementById("entry.714291970").value = "";
+                $scope.messageSent = true;
             }
         }]
     });
