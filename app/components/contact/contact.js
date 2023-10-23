5bb8f2328286987ee5f5f6c47f4344c15e52e65a
Lucian Voica <lucianbookster@outlook.com>
2023-10-23 10:30:08 +0300

    try contact refresh

--- app/components/contact/contact.js
+++ app/components/contact/contact.js
@@ -1,7 +1,11 @@
 angular.module('app')
     .component('contact', {
         templateUrl: "app/components/contact/contact.html",
-        controller: [function () {
-            
+        controller: ['$scope', function ($scope) {
+            $scope.resetForm = function() {
+                document.getElementById("entry.993355435").value = "";
+                document.getElementById("entry.488430138").value = "";
+                document.getElementById("entry.714291970").value = "";
+            }
         }]
     });
